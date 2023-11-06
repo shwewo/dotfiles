3d11a0a964289147f5c06b6b4162bdf36a6f9635
shwewo <shwewo@gmail.com>
2023-11-06 08:44:35 +0300

    neko cred

--- modules/oracle-cloud/age.nix
+++ modules/oracle-cloud/age.nix
@@ -6,6 +6,7 @@
     socks = { file = ../../secrets/oracle-cloud/socks.age; owner = "socks"; group = "socks"; };
     reality = { file = ../../secrets/oracle-cloud/reality.age; owner = "socks"; group = "socks"; };
     wireguard = { file = ../../secrets/oracle-cloud/wireguard.age; owner = "root"; group = "root"; };
+    neko = { file = ../../secrets/oracle-cloud/neko.age; owner = "root"; group = "root"; }
   };
 
   age.identityPaths = [ "/home/cute/.ssh/id_ed25519" ];
