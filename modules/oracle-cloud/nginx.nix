c9e7b9ab29e176501fa0584a10d08cd65464e0f3
shwewo <shwewo@gmail.com>
2023-11-06 09:05:31 +0300

    internal nginx file serving

--- modules/oracle-cloud/nginx.nix
+++ modules/oracle-cloud/nginx.nix
@@ -9,7 +9,7 @@
   system.activationScripts.nginx_filebrowser = ''
     if [ ! -d "/var/www/filebrowser" ]; then
       ${pkgs.git}/bin/git clone https://github.com/mohamnag/nginx-file-browser /var/www/filebrowser
-      chmod 770 /var/www/filebrowser
+      chmod -R 555 /var/www/filebrowser
       chown nginx:nginx /var/www/filebrowser
     fi
   '';
@@ -29,6 +29,16 @@
     '';
     locations."/".extraConfig = ''
       root /var/www/filebrowser;
+    '';
+  };
+
+  services.nginx.virtualHosts."internal" = {
+    forceSSL = false;
+    listen = [{port = 35782;  addr="127.0.0.1"; ssl=false;}];
+    root = "/var/www/internal";
+    locations."/".extraConfig = ''
+      try_files $uri $uri/ =404;
+      autoindex on;
     '';
   };
 }
