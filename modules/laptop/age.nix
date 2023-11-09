f137946fdc62e5143aeb399ffdd5b8e22852d953
shwewo <shwewo@gmail.com>
2023-11-09 13:03:06 +0300

    my hed hurs

--- modules/laptop/age.nix
+++ modules/laptop/age.nix
@@ -8,13 +8,13 @@
     socks_v2ray_turkey = { file = ../../secrets/laptop/socks_v2ray_turkey.age; owner = "socks"; group = "socks"; };
     socks_v2ray_canada = { file = ../../secrets/laptop/socks_v2ray_canada.age; owner = "socks"; group = "socks"; };
     socks_v2ray_france = { file = ../../secrets/laptop/socks_v2ray_france.age; owner = "socks"; group = "socks"; };
-    wireguard_sweden = { file = ../../secrets/laptop/wireguard_sweden.age; owner = "root"; group = "root"; };
-    cloudflared = { file = ../../secrets/laptop/cloudflared.age; owner = "cloudflared"; group = "cloudflared"; };
+    # wireguard_sweden = { file = ../../secrets/laptop/wireguard_sweden.age; owner = "root"; group = "root"; };
+    # cloudflared = { file = ../../secrets/laptop/cloudflared.age; owner = "cloudflared"; group = "cloudflared"; };
     rclone = { file = ../../secrets/laptop/rclone.age; owner = "cute"; group = "users"; }; 
     backup = { file = ../../secrets/laptop/backup.age; owner = "cute"; group = "users"; }; 
     precise = { file = ../../secrets/laptop/precise.age; owner = "cute"; group = "users"; };
-    blogrs = { file = ../../secrets/laptop/.blogrs.age; owner = "cute"; group = "users"; };
-    blogrs_webhook = { file = ../../secrets/laptop/.blogrs_webhook.age; owner = "cute"; group = "users"; };
+    # blogrs = { file = ../../secrets/laptop/.blogrs.age; owner = "cute"; group = "users"; };
+    # blogrs_webhook = { file = ../../secrets/laptop/.blogrs_webhook.age; owner = "cute"; group = "users"; };
   };
 
   age.identityPaths = [ "/home/cute/.ssh/id_ed25519" ];
