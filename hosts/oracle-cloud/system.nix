1d3b8be65737b41ebab78a072ae55ca2fbe8fd79
shwewo <shwewo@gmail.com>
2023-11-30 01:28:31 +0300

    dcef work dir

--- hosts/oracle-cloud/system.nix
+++ hosts/oracle-cloud/system.nix
@@ -59,9 +59,10 @@
       };
       dcef = {
         image = "docker.io/slada/dcef:main";
-        workdir = "/var/www/html/discord_data";
+        workdir = "/discord_data";
         volumes = [
           "dcef_cache:/dcef/cache"
+          "/var/www/html/discord_data:/discord_data"
         ];
         ports = [
           "127.0.0.1:21011:21011"
