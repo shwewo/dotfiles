a9177a0b12c429389afe72283bde811f1a98eea6
shwewo <shwewo@gmail.com>
2024-02-03 08:23:10 +0300

    socks

--- modules/laptop/socks.nix
+++ modules/laptop/socks.nix
@@ -15,7 +15,7 @@
     { name = "socks-v2ray-sweden";   script = "ss-local -c /run/agenix/socks_v2ray_sweden"; } # port 1080
     { name = "socks-v2ray-canada";   script = "ss-local -c /run/agenix/socks_v2ray_canada"; } # port 1081
     { name = "socks-v2ray-france";   script = "ss-local -c /run/agenix/socks_v2ray_france"; } # port 1082
-     { name = "socks-v2ray-turkey";   script = "ss-local -c /run/agenix/socks_v2ray_turkey"; } # port 1083
+    { name = "socks-v2ray-turkey";   script = "ss-local -c /run/agenix/socks_v2ray_turkey"; } # port 1083
     { name = "socks-reality-sweden"; script = "sing-box run --config /run/agenix/socks_reality_sweden"; } # port 2080
   ];
 in {
