c5f7720b2e5dbdb5a800126dac3710aa082715d6
shwewo <shwewo@gmail.com>
2024-03-06 15:45:41 +0300

    :3

--- hosts/oracle-cloud/system.nix
+++ hosts/oracle-cloud/system.nix
@@ -98,5 +98,4 @@
   services.adguardhome.enable = true;
   services.adguardhome.settings.bind_port = 4000;
   services.adguardhome.settings.bind_host = "100.122.26.102";
-  services.adguardhome.mutableSettings = false;
 }
