354f277149c88515512ef38e6f5828f5b2103244
shwewo <shwewo@gmail.com>
2025-11-13 05:40:47 +0100

    immich intermediate update

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     rolling.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    immich_intermediate = "github:nixos/nixpkgs?rev=46224bfbaeaeea01782d54e3dda5bfb4af94309d";
+    immich_intermediate.url = "github:nixos/nixpkgs?rev=46224bfbaeaeea01782d54e3dda5bfb4af94309d";
 
     secrets.url = "git+ssh://git@github.com/shwewo/secrets";
     agenix.url = "github:ryantm/agenix";
